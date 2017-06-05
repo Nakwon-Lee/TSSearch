8f4269a095de9138dd5db72f7c40e7c428e8e72b
skrdnjs <skrdnjs0904@naver.com>
2017-06-05 11:35:47 +0900

    initial commit

---
+++ TSSearch.py
@@ -0,0 +1,15 @@
+def TSSearch():
+
+	bestts = None
+
+	while(1):
+		#TODO Make a new solution
+		# local search
+		# make a new search strategy formula (total order)
+
+		#TODO Calculate the fitness of the new solution
+		# execution of cpachecker with new total order
+
+		#TODO If the new solution is better than the best solution, change the solution
+
+	return bestts
