6e842bb1376480637c8364f893a1d5b5a0694e0f
skrdnjs <skrdnjs0904@naver.com>
2017-06-12 23:58:51 +0900

    170612 11:59 random order generation, odr to xml

--- TSSearch.py
+++ TSSearch.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import glob
 import os
 import sys
+from xml.etree.ElementTree import Element, SubElement, dump
 
 sys.dont_write_bytecode = True # prevent creation of .pyc files
 for egg in glob.glob(os.path.join(os.path.dirname(__file__), os.pardir, 'lib', 'python-benchmark', '*.egg')):
@@ -54,10 +55,10 @@ def makingAtomTotalOrders(labfuncs):
 
 	for labfunc in labfuncs:
 		if labfunc[1] == 1: #TtOdr with finite domain
-			ato = FiniteDomainTotalOrder(labfunc[0],labfunc[2])
+			ato = FiniteDomainTotalOrder(labfunc[0],labfunc[3],labfunc[2])
 			atos.append(ato)
 		elif labfunc[1] == 0: #TtOdr with infinite domain
-			ato = AtomicTotalOrder(labfunc[0])
+			ato = AtomicTotalOrder(labfunc[0],labfunc[2])
 			atos.append(ato)
 		else:
 			print('labfunc should be finite or infinite domain')
@@ -65,7 +66,42 @@ def makingAtomTotalOrders(labfuncs):
 	return atos
 
 def ttOdrToXML(pttOdr,pxmlfile):
-	pass
+	ttodrelem = Element('ttOdr')
+	xmlDFS(pttOdr.toroot,ttodrelem)
+	indent(ttodrelem)
+	dump(ttodrelem)
+
+def xmlDFS(curr,elem):
+	ato = curr.ato
+	subelem = Element(curr.ato.name)
+	subelem.attrib['Odr'] = str(curr.ato.odr)
+	elem.append(subelem)
+	if isinstance(ato,FiniteDomainTotalOrder):
+		for i in range(len(curr.children)):
+			subelem2 = Element(curr.ato.name + ' switch')
+			subelem2.attrib['Domain'] = str(curr.ato.domain[i])
+			elem.append(subelem2)
+			xmlDFS(curr.children[i],subelem2)
+	elif isinstance(ato,AtomicTotalOrder):
+		subelem3 = Element(curr.ato.name + ' else')
+		elem.append(subelem3)
+		if len(curr.children) is not 0:
+			xmlDFS(curr.children[0],subelem3)
+
+def indent(elem, level=0):
+    i = '\n' + level*'  '
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
 
 def ttOdrToJAVA(pttOdr,pJavafile):
 	pass
@@ -74,16 +110,24 @@ def main():
 
 	print('Traversal Strategy Search Start')
 
+	currts = None
 	bestts = None
 	currvals = None
 	outlog = 'output.log'
 	fitvars = ('NoAffS','VL','VC')
-	labfuncs = (('IsAbs',1,(0,1),0),('blkD',0,0),('blkD',0,1),('CS',0,0),('CS',0,1),('RPO',0,0),('RPO',0,1),('uID',0,0),('uID',0,1))
+	labfuncs = (('IsAbs',1,(0,1),0),('blkD',0,0),('CS',0,0),('CS',0,1),('RPO',0,0),('RPO',0,1),('uID',0,0),('uID',0,1))
 	atos = None
 
 	atos = makingAtomTotalOrders(labfuncs)
 
-	print(atos)
+	#print(atos)
+
+	currts = TraversalStrategy(atos)
+	currts.randomOdrGen()
+
+	currts.printTS()
+
+	ttOdrToXML(currts,1)
 
 	while(1):
 		#TODO Make a new solution
